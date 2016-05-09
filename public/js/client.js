5ea0e353f8c7b8391f567770aadf4c80b27df710
laskeym <laskey.mark@hotmail.com>
2016-05-09 10:14:23 -0400

    first commit

--- public/js/client.js
+++ public/js/client.js
@@ -1,88 +1,126 @@
-jQuery(function($){
-			var socket = io.connect();
-			var $nickForm = $('#setNick');
-			var $nickBox = $('#nickname');
-			var $nickError = $('#nickError');
-			var $users = $('#userlist');
-			var $messageForm = $('#send-message');
-			var $messageBox = $('#message');
-			var $chat = $('#chat');
-			var height = 0;
-			var connected = false;
+$(document).ready(function (){
+	var socket = io.connect();
+	var $nickForm = $('#setNick');
+	var $nickBox = $('#nickname');
+	var $nickError = $('#nickError');
+	var $users = $('#userlist');
+	var $messageForm = $('#send-message');
+	var $messageBox = $('#message');
+	var $chat = $('#chat');
+	var connected = false;
+		
+	/*---Navigation through website start---*/
+	$("#about-link").click(function(){
+		if($(this).hasClass("active")){
+			$("#aboutpage").show(2000);
+			$(".welcome-page").hide();
+			$("body").css("background-image", "none");
+			$("#chat-container").hide();
+		}
+	});
+
+	$("#home-link").click(function(){
+		if($(this).hasClass("active")){
+			$(".welcome-page").show(2000);
+			$("#chat-container").hide();
+			$("body").css("background-image", "url('http://njtod.org/wp-content/uploads/2015/03/INDG_coworkingspacewlibrary_3-copy.jpg')");
+		}
+	});
+
+	$("#enterchat").click(function(e){	
+		e.preventDefault();
+		if(!connected){
+			$("#main-content").show(2000);
+			$("#aboutpage").hide();
+			$(".welcome-page").hide(2000);
+			$("#chat-container").hide();
+			$("#nickname").focus();
+		}
+		else{
+			e.preventDefault();
+			$("#main-content").hide();
+			$("#aboutpage").hide();
+			$(".welcome-page").hide();
+			$("#chat-container").show(2000);
+			$("#message").focus();
+		}
+	});
+	/*---Navigation through website end---*/
 			
-			//adds red font to the error message when a username is taken
-			document.getElementById("nickError").style.color = "red";
+	//adds red font to the error message when a username is taken
+	document.getElementById("nickError").style.color = "red";
 			
+	//scrolls conversation screen to bottom upon data input
+	$("#chat").bind("DOMSubtreeModified",function(){
+		$("#chat").animate({
+			scrollTop: $("#chat")[0].scrollHeight
+		});
+	});
+	
+	//function that eliminates special characters on the username input screen
+	$('#nickname').bind('keypress', function (event){
+		var regex = new RegExp(/[a-zA-Z0-9]+/);
+		var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+		if (!regex.test(key)){
+			event.preventDefault();
 			
-			//this function doesn't allow white space upon entering username
-			$(function() {
-				$('#nickname').on('keypress', function(e) {
-					if (e.which == 32)
-						return false;
-				});
-			});
+			return false;
+		}
+	});
+	
+	//Username create button functionality
+	$nickForm.submit(function(e){
+		e.preventDefault();
+		socket.emit('new user', $nickBox.val(), function(data){
+			if(data){
+				$("#main-content").hide();
+				$("#chat-container").show(2000);
+				$("#message").focus();
+				connected = true;
+			}
+			else{
+				$nickError.html('Invalid username!');
+			}
+		});
+		$nickBox.val('');
+	});
 			
+	//Function that deals with the message sent by a user in the chat
+	$messageForm.submit(function(e){
+		e.preventDefault();
+		socket.emit('send message', $messageBox.val(), function(data){
+			$chat.append('<span class = "error"><b>' +data+ "</span><br/>");
+		});
+		$messageBox.val('');
+	});
 			
-			$('#chat_container').hide(); //hide the chat window until username is entered
-			$nickForm.submit(function(e){
-				e.preventDefault();
-				socket.emit('new user', $nickBox.val(), function(data){
-					if(data){
-						$('#nickWrap').hide();
-						$('#chat_container').show();
-						$("body").css("background-image", "url('')");
-						$("body").css("background-color", "#2c3e50")
-						$('.lead').hide();
-						connected = true;
-					}else{
-						$nickError.html('Invalid username!');
-					}
-				});
-				$nickBox.val('');
-			});
+	socket.on('usernames', function(data){ //take the nickname array and add/delete in userlist window
+		var html = '';
+		for(i=0; i < data.length; i++){
+			html += '<li class=\"list-group-item\"><span>' + '<b>' + data[i] + '</b>' + '</span> <i class=\"fa fa-' + '\"></i> ' + '<br/>';
+		}
+		$users.html(html);
+	});
 			
-			$('#chat').each(function(i, value){
-				height += parseInt($(this).height());
-			});
-			height += '';
+	socket.on("update1", function(msg){
+		$chat.append("<b>" + msg + "</b><br>");
+	});
 			
-			$messageForm.submit(function(e){
-				e.preventDefault();
-				
-				socket.emit('send message', $messageBox.val(), function(data){
-					$chat.append('<span class = "error"><b>' +data+ "</span><br/>");
-					$('#chat').stop(true, false).animate({scrollTop: height});
-				});
-				$messageBox.val('');
-				
-			});
+	socket.on("update2", function(msg){
+		if (connected)
+			$chat.append('<span class = "update2"><b>' +msg+ "</span><br>");
+	});
 			
-			socket.on('usernames', function(data){ //take the nickname array and add/delete in userlist window
-				var html = '';
-				for(i=0; i < data.length; i++){
-					html += '<b>' + data[i] + '</b>' + '<br/>';
-				}
-				$users.html(html);
-			});
+	socket.on("dcupdate", function(msg){
+		if (connected)
+			$chat.append('<span class = "update2"><b>' +msg+ "</span><br>");
+	});
 			
-			socket.on("update1", function(msg){
-				$chat.append("<b>" + msg + "</b><br>");
-			});
-			
-			socket.on("update2", function(msg){
-				$chat.append('<span class = "update2"><b>' +msg+ "</span><br>");
-				$('#chat').stop(true, false).animate({scrollTop: height});
-			});
-			
-			
-			socket.on("dcupdate", function(msg){
-				if (connected){
-				$chat.append('<span class = "update2"><b>' +msg+ "</span><br>");
-				$('#chat').stop(true, false).animate({scrollTop: height});}
-			});
-			
-			socket.on('new message', function(data){
-				$chat.append("<li><strong><span class='text-success'>" + data.nick + "</span></strong>: " + data.msg + "</li>"); //sends to the chat window Username: Message
-				$('#chat').stop(true, false).animate({scrollTop: height});
-			});
-		});	
+	socket.on('new message', function(data){
+		$chat.append("<li id = 'msg'><strong><span class='text-success'>" + data.nick + ":</span></strong> " + data.msg + "</li>"); //sends to the chat window Username: Message
+	});
+	
+	socket.on('whisper', function(data){
+		$chat.append("<span class='whisper'><li id = 'whisper'><strong><span class='text-success'>" + data.nick + ":</span></strong> " + data.msg + "</li></span>");
+	});
+});	
--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	nicknames = [];
+var express = require('express');
+var	app = express();
+var server = require('http').createServer(app);
+var	io = require('socket.io').listen(server);
+var url = require('url');
+var	users = {};
 	
-server.listen(3000);
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/public/index.html');
+var port = Number(process.env.PORT || 3000);
+server.listen(port, function(){
+	console.log('Listening on port: ' + port);
 });
 
+//define the public folder for the app
 app.use(express.static(__dirname + '/public'));
 
+//define the webpage routes
+function app(req, res){
+	var path = url.parse(req.url).pathname;
 
+	switch(path){
+		case '/about':
+			break;
+		default:
+			app.get('/', function(req, res){
+				res.send(__dirname + '\public\index.html');
+			});
+			break;
+	}
+}
+
+//server code start
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
-		if(nicknames.indexOf(data) != -1){	//if the nickname(data) entered exists in array, emit error 
+		if(data in users){	//if the nickname(data) entered exists in array, emit error 
 			callback(false);
 		} 
 		else if (data.trim() === '')
@@ -23,32 +39,53 @@ io.sockets.on('connection', function(socket){
 		else{ 
 			callback(true);
 			socket.nickname = data; //assign the nickname
-			nicknames.push(socket.nickname); //push the nickname to the array
+			users[socket.nickname] = socket;
+			//nicknames.push(socket.nickname); //push the nickname to the array
 			socket.emit("update1", "You have connected to the server.");
 			io.sockets.emit("update2", socket.nickname + " has connected to the server.");
-			io.sockets.emit('usernames', nicknames); //send data to client side
+			io.sockets.emit('usernames', Object.keys(users)); //send data to client side
 		}
 	});
 	
 	socket.on('send message', function(data, callback){
 		var msg = data.trim();// takes care of empty white space before a message is sent
-		
-			// Below is the whisper aka private message function
+			//start
+			/*if(msg.substr(0,3) === '/w '){
+				msg = msg.substr(3);
+				var ind = msg.indexOf(' ');
+				if(ind !== -1){
+					var name = msg.substr(0, ind);
+					msg = msg.substr(ind + 1);
+					if (name in users){
+						users[name].emit('whisper', {msg: msg, nick: socket.nickname
+						});
+					}
+					else{
+						callback("error!  Enter a valid user!");
+					}
+				}
+				else{
+					callback("Error! Please enter a message for your whisper.");
+				}
+			}*/
+			//end
 			if(msg.substr(0) === ''){   //if the message being sent is equal to 0 characters
 				callback('Error! Invalid Message');
-			}else{
-			io.sockets.emit('new message', {msg: msg, nick: socket.nickname}); //sends multiple data.  first argument is the message currently being sent by the client, the second argument is the username of the client
 			}
-	
-	
+			else{
+				io.sockets.emit('new message', {msg: msg, nick: socket.nickname}); //sends multiple data.  first argument is the message currently being sent by the client, the second argument is the username of the client
+			}
+			
 	}); //Revisit to fix callback functionalities
 	
 	socket.on('disconnect', function(data){
 		if(!socket.nickname) //if data doesn't match, return to initial state
 			return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1); //delete the user from list by grabbing the element in the array where particular user's data is located 
+		
+		delete users[socket.nickname];
+		//nicknames.splice(nicknames.indexOf(socket.nickname), 1); //delete the user from list by grabbing the element in the array where particular user's data is located 
 		io.sockets.emit("dcupdate", socket.nickname + " has disconnected from the server.")
-		io.sockets.emit('usernames', nicknames); //send data to client side
+		io.sockets.emit('usernames', users); //send data to client side
 	});
 });
 
